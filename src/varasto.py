b6c5f168700577ce4be195ed144178e8c6fb383f
Vesa Jolkkonen <vesa.jolkkonen@hotmail.com>
2023-11-06 18:26:36 +0200

    Testit rikkova muutos versio 2

--- src/varasto.py
+++ src/varasto.py
@@ -21,7 +21,7 @@ class Varasto:
         return self.tilavuus - self.saldo
 
     def lisaa_varastoon(self, maara):
-        self.varasto.saldo += maara
+        self.saldo += maara
         if maara < 0:
             return
         if maara <= self.paljonko_mahtuu():
